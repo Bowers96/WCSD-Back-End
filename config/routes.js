670b44a26e15458384ced10ed3371a03809878f1
Bowers96 <bowersc96@gmail.com>
2017-01-18 09:27:09 -0500

    routes

--- config/routes.js
+++ config/routes.js
@@ -2,20 +2,28 @@
 
 module.exports = require('lib/wiring/routes')
 
-// create routes
+  // create routes
 
-// what to run for `GET /`
-.root('root#root')
+  // what to run for `GET /`
+  .root('root#root')
 
-// standards RESTful routes
-.resources('examples')
+  // standards RESTful routes
+  .resources('examples')
+  .patch('carts', 'carts#update')
+  .resources('carts')
 
-// users of the app have special requirements
-.post('/sign-up', 'users#signup')
-.post('/sign-in', 'users#signin')
-.delete('/sign-out/:id', 'users#signout')
-.patch('/change-password/:id', 'users#changepw')
-.resources('users', { only: ['index', 'show'] })
+
+
+
+  // users of the app have special requirements
+  .post('/sign-up', 'users#signup')
+  .post('/sign-in', 'users#signin')
+  .delete('/sign-out/:id', 'users#signout')
+  .patch('/change-password/:id', 'users#changepw')
+  .resources('users', {
+    only: ['index', 'show']
+  })
+
 
 // all routes created
 ;
